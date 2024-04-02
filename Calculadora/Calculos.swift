6cd96d19a3c32165a5d39aaea362354eb6ddcdd1
Luis <luis.fernandomunoz@plexus.es>
2024-04-02 16:33:37 +0200

    Resuelto teclado calculadora

---
+++ Calculadora/Calculos.swift
@@ -0,0 +1,8 @@
+//
+//  Calculos.swift
+//  Calculadora
+//
+//  Created by Luis Fernando Sanchez Muñoz on 2/4/24.
+//
+
+import Foundation
---
+++ Calculadora/ComponentesUI.swift
@@ -0,0 +1,8 @@
+//
+//  ComponentesUI.swift
+//  Calculadora
+//
+//  Created by Luis Fernando Sanchez Muñoz on 2/4/24.
+//
+
+import Foundation
